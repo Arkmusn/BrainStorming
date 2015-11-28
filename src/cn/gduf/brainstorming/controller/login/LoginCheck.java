5acc6c2e921fad030f96a49ee6f46fa138ca5b01
Arkmusn <ark.c.mf@gmail.com>
2015-11-28 11:48:27 +0800

    登录模块校验初步完成 未测试

--- src/cn/gduf/brainstorming/controller/login/LoginCheck.java
+++ src/cn/gduf/brainstorming/controller/login/LoginCheck.java
@@ -45,4 +45,50 @@ public abstract class LoginCheck {
 		return flag;
 	}
 
+	/**
+	 * 校验传入的用户名是否已注册
+	 * 
+	 * @param user
+	 *            - User实体类
+	 * @return 是否已注册
+	 */
+	public static boolean isUserExisted(User user) {
+		boolean flag = false;
+
+		try {
+			user = DAOFactory.getLoginDAOInstance().findByName(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		String userName = null;
+		userName = user.getUserName();
+		// 若用户存在
+		if (!(userName == null || userName.isEmpty())) {
+			flag = true;
+		}
+
+		return flag;
+	}
+
+	/**
+	 * 传入用户实体获得用户密码
+	 * 
+	 * @param user
+	 *            - User实体类
+	 * @return 用户密码
+	 */
+	public static String getUserPassword(User user) {
+		String userPassword = null;
+
+		try {
+			user = DAOFactory.getLoginDAOInstance().findByName(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		userPassword = user.getUserPassword();
+		return userPassword;
+	}
+
 }
--- src/cn/gduf/brainstorming/controller/login/servlet/LoginServlet.java
+++ src/cn/gduf/brainstorming/controller/login/servlet/LoginServlet.java
@@ -26,20 +26,62 @@ public class LoginServlet extends HttpServlet {
 
 		String strOut = null;
 		JsonObject json = new JsonObject();
-		
-		User userTemp = new User();
-		
-		String userName = request.getParameter("username");
-		String userPassword = request.getParameter("password");
-		userTemp.setUserName(userName);
-		userTemp.setUserPassword(userPassword);
-		
-		boolean flag = LoginCheck.isIdentifiedUser(userTemp);
-		
-		
+
+		String type = request.getParameter("type");
+
+		// type - 验证类型
+		// 0 - 接受用户名返回密码
+		// 1 - 常规登录验证
+		// 2 - 校验用户名是否存在
+		// 接受用户名返回密码
+		if (type.equals("0")) {
+			User userTemp = new User();
+
+			String userName = request.getParameter("username");
+			userTemp.setUserName(userName);
+
+			// 获取密码
+			String userPassword = LoginCheck.getUserPassword(userTemp);
+			json.add("password", userPassword);
+			strOut = json.toString();
+		}
+		// 常规登录验证
+		else if (type.equals("1")) {
+			User userTemp = new User();
+
+			String userName = request.getParameter("username");
+			String userPassword = request.getParameter("password");
+			userTemp.setUserName(userName);
+			userTemp.setUserPassword(userPassword);
+
+			boolean flag = LoginCheck.isIdentifiedUser(userTemp);
+
+			if (flag == true) {
+				json.add("success", 1);
+			} else {
+				json.add("success", 0);
+			}
+			strOut = json.toString();
+		}
+		// 校验用户名是否存在
+		else if (type.equals("2")) {
+			User userTemp = new User();
+
+			String userName = request.getParameter("username");
+			userTemp.setUserName(userName);
+
+			boolean flag = LoginCheck.isUserExisted(userTemp);
+
+			if (flag == true) {
+				json.add("registerFlag", 1);
+			} else {
+				json.add("registerFlag", 0);
+			}
+			strOut = json.toString();
+		}
+
 		PrintWriter out = response.getWriter();
 		out.println(strOut);
 
 	}
-
 }
